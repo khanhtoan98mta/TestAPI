869205bf0f42be1c5a1fca5d46f9cba5ce3d12f6
KhanhToan <KhanhToan@DESKTOP-C6JGQK7>
2020-06-03 15:52:54 +0700

    a

--- app/src/main/java/com/example/testapi/MainActivity.java
+++ app/src/main/java/com/example/testapi/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.testapi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -10,51 +11,78 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.json.JSONArray;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
+
+import static android.widget.Toast.*;
+import static android.widget.Toast.LENGTH_LONG;
 
 
 public class MainActivity extends AppCompatActivity {
     String url="httpss://localhost:8081/hoadons";
     TextView txtJson;
     Button btnClickme;
+    ArrayList<mathang> listmh;
+    String res;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         txtJson = (TextView) findViewById(R.id.txtJson);
         btnClickme= findViewById(R.id.btn_click);
-        final String myjson="";
 
-        OkHttpClient client = new OkHttpClient();
-        Request request=new Request.Builder().url("http://localhost:8081/hoadons").build();
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                Log.e("Loi","Loi");
-            }
 
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                final String json=response.body().string();
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        txtJson.setText(json);
-                    }
-                });
-            }
-        });
+
+//                getData("http://35.198.237.116/coffeshop/api/mathangs");
+        new getURL().execute("http://35.198.237.116/coffeshop/api/mathangs");
 
     }
+class getURL extends AsyncTask<String,Void,String>
+{
+    OkHttpClient client=new OkHttpClient.Builder().connectTimeout(15, TimeUnit.SECONDS)
+                            .writeTimeout(15,TimeUnit.SECONDS)
+                            .readTimeout(15,TimeUnit.SECONDS)
+                            .retryOnConnectionFailure(true)
+                            .build();
+    @Override
+    protected String doInBackground(String... strings) {
+        Request.Builder builder = new Request.Builder();
+        builder.url(strings[0]);
+        Request request=builder.build();
+        try {
+            Response response= client.newCall(request).execute();
+            return response.body().string();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        if(!s.equals(""))
+        {
+
+    }
+
+        super.onPostExecute(s);
+    }
+}
 
 
 
---
+++ app/src/main/java/com/example/testapi/mathang.java
@@ -0,0 +1,37 @@
+package com.example.testapi;
+
+public class mathang {
+    private int id;
+    private String donvitinh;
+    private double gia;
+    private String tenhang;
+
+    public mathang(int id, String donvitinh, long gia, String tenhang) {
+        this.id = id;
+        this.donvitinh = donvitinh;
+        this.gia = gia;
+        this.tenhang = tenhang;
+    }
+    public mathang()
+    {
+
+    }
+
+
+
+
+    public String getDonvitinh() { return donvitinh; }
+    public void setDonvitinh(String value) { this.donvitinh = value; }
+    public double getGia() { return gia; }
+    public void setGia(double value) { this.gia = value; }
+    public String getTenhang() { return tenhang; }
+    public void setTenhang(String value) { this.tenhang = value; }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getId()
+    {
+        return id;
+    }
+}
